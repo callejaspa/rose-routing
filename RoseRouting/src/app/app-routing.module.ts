5e3bd35cfc640083b06b2a9497c5b38e28b62bf6
Pauline Callejas <callejaspa@gmail.com>
2018-02-14 12:53:18 -0500

    Fundamentals of routing

--- RoseRouting/src/app/app-routing.module.ts
+++ RoseRouting/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
+import { SigninComponent } from './+signin/signin.component';
+import { HomeComponent } from './+home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
